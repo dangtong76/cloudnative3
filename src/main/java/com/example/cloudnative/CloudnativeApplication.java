e0a1c99675227d4e687ee45e2fcebace9ea3e635
dangtong76 <dangtong@gmail.com>
2023-12-02 16:12:09 +0900

    change hello world message

--- src/main/java/com/example/cloudnative/CloudnativeApplication.java
+++ src/main/java/com/example/cloudnative/CloudnativeApplication.java
@@ -12,7 +12,7 @@ public class CloudnativeApplication {
     
 	@RequestMapping("/")
 	String home() {
-		return "Hello World!!";
+		return "Hello World!! ok?";
 	}
 
 	public static void main(String[] args) {
